6f0967e055f0d8a536a4e63d2d1b24f5d3a55027
VeganGuy1 <pikarescuer@gmail.com>
2026-05-28 11:50:33 -0400

    martin is mean

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const scrollContainer = document.querySelector(".testclass");
 
 scrollContainer.addEventListener("wheel", (evt) => {
-  evt.preventDefault(); // Prevents the page from scrolling vertically
+  evt.preventDefault(true); // Prevents the page from scrolling vertically
   scrollContainer.scrollLeft += evt.deltaY;
 });
